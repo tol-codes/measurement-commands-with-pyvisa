5852e4535a1c24b324a24ad432fbb133b0e96c3f
tol-codes <tanakaohya.github@gmail.com>
2022-04-04 21:47:03 +0900

    Add notebook measurment commands

---
+++ common/config.py
@@ -0,0 +1,3 @@
+devices = {"GPIB0::3::INSTR" : 'TAKASAGO,AP-1628T,00000,Ver2.30Rev1.06\r\n', 
+            "GPIB0::4::INSTR" : 'Keithley Instruments Inc., Model 2636A, 1252354, 2.1.3\r\n',
+            "GPIB0::26::INSTR" : 'TAKASAGO,AP-1628T,00000,Ver2.30Rev1.06\r\n'}
