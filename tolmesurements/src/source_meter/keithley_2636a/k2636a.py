9d448750cc2bb4f4a912d472f59949f6522bcd9c
tol-codes <tanakaohya.github@gmail.com>
2022-04-20 18:47:50 +0900

    Update k2636a.py

---
+++ tolmesurements/src/source_meter/keithley_2636a/k2636a.py
@@ -0,0 +1,25 @@
+from tolmesurements import Instrument
+from tolmesurements.src.instrument import InstrumentConfig
+
+class K2636A(Instrument):
+    def __init__(self, instrConfig):
+        super().__init__(instrConfig)
+
+    def reset(self):
+        self.controller.write("smua.reset()")
+        self.controller.write("smub.reset()")
+
+    def getValue(self):
+        pass
+
+    def setValue(self, value):
+        value = (value * 3200).round().astype(np.int16)
+        self.controller.write(f'A1D{value}')
+    
+    def setRange(self, value):
+        ch = value["ch"]
+        rangev = value["v"]
+        rangei = value["i"]
+
+        self.controller.write(f'smu{ch}.measure.rangev = {rangev}')
+        self.controller.write(f'smu{ch}.measure.rangev = {rangei}')
