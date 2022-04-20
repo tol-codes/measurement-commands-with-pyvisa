d96014cb12e097af13c2e702a2cd3216efdf2a24
tol-codes <tanakaohya.github@gmail.com>
2022-04-20 18:47:53 +0900

    Update magnet.py

--- tolmesurements/src/magnet/magnet.py
+++ tolmesurements/src/magnet/magnet.py
@@ -1,12 +1,19 @@
-import imp
+import numpy as np
+import time
 from tolmesurements import Instrument
+from tolmesurements.src.instrument import InstrumentConfig
 
 class Magnet(Instrument):
     def __init__(self, instrConfig):
         super().__init__(instrConfig)
 
     def reset(self):
-        pass
+        self.controller.write("A1D0")
 
     def getValue(self):
-        return float(self.controller.query('KRDG? A'))
+        pass
+
+    def setValue(self, value):
+        value = (value * 3200).round().astype(np.int16)
+        self.controller.write(f'A1D{value}')
+        time.sleep(1)
