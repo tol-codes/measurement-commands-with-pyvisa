0833c76473a7b003ac0b5a5fd901fcfa98d0be9b
tol-codes <tanakaohya.github@gmail.com>
2022-04-21 15:48:32 +0900

    Revise the folder configulation

--- tolmesurements/__init__.py
+++ tolmesurements/__init__.py
@@ -1,8 +1,2 @@
 ### Temperature Controller
-from .src.temperature_controller import temperature_controller
-
-### Magnet
-from .src.magnet import magnet
-
-### keithley 2636A
-from .src.source_meter.keithley_2636a import k2636a
+from .src import *
